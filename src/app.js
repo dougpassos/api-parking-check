8a587517bd615d778686c0ad3f72dfcc47cd30d1
Douglas Passos <douglas.p.doug@gmail.com>
2023-07-14 23:36:59 -0300

    ajuste app

--- src/app.js
+++ src/app.js
@@ -1,8 +1,9 @@
 const express = require('express');
-const routes = require('./routes/routes')
 const app = express();
+const router = require('./routes/router')
+app.use(express.json())
 
-app.use(routes);
+app.use("/api", router);
 
 const conn = require('./db/conn')
 conn();
