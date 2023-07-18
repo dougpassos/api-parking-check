28ac6c43fd7199049a31c7a8c773f85f96ffab44
Douglas Passos <douglas.p.doug@gmail.com>
2023-07-17 22:39:00 -0300

    add regra registrar entra e saida 1x

--- src/controlles/parkingController.js
+++ src/controlles/parkingController.js
@@ -7,8 +7,17 @@ const parkingController = {
         plateVehicle: req.body.plateVehicle,
         entryData: req.body.entryData
       };
-
-      const response = await ParkingModel.create(parking);
+      let response = await ParkingModel.findOne({plateVehicle: parking.plateVehicle, outputData: {$exists: false}});
+      if (response) {
+        res.status(406).json(
+          {
+            response,
+            msg: "vehicle is now parked in the parking lot"
+          }
+        );
+        return;     
+      };
+      response = await ParkingModel.create(parking);
       res.status(201).json({ response, msg: "Entrada do veiculo registrada com sucesso"})
       
     } catch (error) {
@@ -21,7 +30,7 @@ const parkingController = {
       const parking = {
         outputData: req.body.outputData
       }
-      const vehicle = await ParkingModel.findOne({plateVehicle: plateVehicle});
+      const vehicle = await ParkingModel.findOne({plateVehicle: plateVehicle,outputData: {$exists: false} });
       if (!vehicle) {
         res.status(404).json(
           {
@@ -40,7 +49,7 @@ const parkingController = {
         );
         return;        
       };
-      const response = await ParkingModel.findOneAndUpdate({plateVehicle: plateVehicle},parking)
+      const response = await ParkingModel.findOneAndUpdate({plateVehicle: plateVehicle, outputData: {$exists: false}},parking)
       res.status(200).json(response);
     } catch (error) {
       console.log(error);    
